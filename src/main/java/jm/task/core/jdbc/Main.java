d1759d4c9e2ff54453a402e5f536daaa5ceeec0e
m-lavrovsky <mark_lv@mail.ru>
2022-01-05 00:50:29 +0300

    task 1.1.3 complete

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+
         UserServiceImpl userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("John","Doe",(byte)12);
